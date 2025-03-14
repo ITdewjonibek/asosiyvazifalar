cec8457e1b06b770edde733597d425d3b5d874ba
Jonibek Jo'rayev Enter <jorayevjonibek63@gmail.com>
2025-03-15 02:59:36 +0500

    ITtelegram faylari

--- App.jsx
+++ App.jsx
@@ -1,83 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Student from './component/Student';
+import Render from './component/Render';
+import Render2 from './component/Render2';
+import userImage from "./component/imgh/image.png"
+import Header from './component/Header';
+import Footer from './component/Footer';
 
-export default function App() {
-    const [users, setUsers] = useState([]);  
-    const [title, setTitle] = useState('');
-      const [text, setText] = useState('');
-    const [number, setNumber] = useState('');
-     const [country, setCountry] = useState('');
-      const [gender, setGender] = useState('');
-    function handleTitleChange(e) {
-        setTitle(e.target.value);
-    }
-    function handleTextChange(e) {
-        setText(e.target.value);
-    }
-    function handleNumberChange(e) {
-        setNumber(e.target.value);
-    }
 
-    function handleCountryChange(e) {
-        setCountry(e.target.value);
-    }
-    function handleGenderChange(e) {
-        setGender(e.target.value);
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        const newUser = { id: Date.now(), title, text, number, country, gender }; 
-        setUsers([...users, newUser]);
-        setTitle('');
-        setText('');
-        setNumber('');
-        setCountry('');
-        setGender('');
-    }
-
-    function handleDelete(userId) {
-        setUsers(users.filter(user => user.id !== userId));
-    }
-
-    return (
-        <div className="read">
-            <form onSubmit={handleSubmit}>
-                <label style={{ background: 'rgb(73, 73, 73)' }}>
-                    <h4>Erkak</h4>
-                    <input type="radio" name="gender" value="Erkak" onChange={handleGenderChange} />
-                </label>
-                <label style={{ background: 'rgb(73, 73, 73)' }}>
-                    <h4>Ayol</h4>
-                    <input type="radio" name="gender" value="Ayol" onChange={handleGenderChange} />
-                </label>
-
-                <input type="text" onChange={handleTitleChange} value={title} placeholder="Firstname" required />
-                <input type="text" onChange={handleTextChange} value={text} placeholder="Lastname" required />
-                <input type="number" onChange={handleNumberChange} value={number} placeholder="Age" required />
-                <select name="hello" id="hello" onChange={handleCountryChange} value={country} required>
-                    <option value="">Country</option>
-                    <option value="Uzbekistan">Uzbekistan</option>
-                    <option value="Russia">Russia</option>
-                    <option value="America">America</option>
-                    <option value="Italy">Italy</option>
-                </select>
-                <button type="submit">Save</button>
-            </form>
-
-            <div className="Run">
-                <h3>users</h3>
-                {users.map((user) => (
-                    <div key={user.id} style={{ border: '1px solid gray', padding: '10px', margin: '10px 0' }}>
-                        <p><strong>Firstname:</strong> {user.title}</p>
-                        <p><strong>Lastname:</strong> {user.text}</p>
-                        <p><strong>Age:</strong> {user.number}</p>
-                        <p><strong>Country:</strong> {user.country}</p>
-                        <p><strong>Gender:</strong> {user.gender}</p>
-                        <button onClick={() => handleDelete(user.id)} style={{ background: 'red', color: 'white' }}>Delete</button>
-                    </div>
-                ))}
-            </div>
+function App() {
+  return (
+    <div className='lot'>
+      <div className='lop1'>
+        <div className='inps'>
+         <div className='input'>
+         <input type="text" placeholder='users search'/>
+         <i class='bx bx-search-alt-2'></i>
+         </div>
         </div>
-    );
+      <Student onle="online" name='Jonibek' imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:2:00" name='Jamshid'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:22:00" name='Jasur'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:12:00" name='Bexruz'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:13:00" name='Daler'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:13:20" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:12:00" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:13:30" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:12:00" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:12:40" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:12:00" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:14:50" name='Mahmud'imgSrc={userImage}></Student>
+      <Student onle="oxirgi marta:13:20" name='Mahmud'imgSrc={userImage}></Student>
+
+      </div>
+      <div className='lop2'>
+        <Header Header='Jonibek' online="online"></Header>
+      <div className="lou">
+      <Render>Asalomu alaykum 👋</Render>
+      <Render2>Asalomu alaykum 👋</Render2>
+      <Render>nima xizmat</Render>
+      <Render2>menga yordam kerak edi</Render2>
+      <Render>siz menga batafsil ayting</Render>
+      <Render2>menga web sayt qilib bering</Render2>
+      <Render>Qancha bujetiz bor</Render>
+      <Render2>1000$</Render2>    
+      </div>
+      <Footer></Footer>
+      </div>
+    </div>
+    
+  );
 }
 
+export default App;
